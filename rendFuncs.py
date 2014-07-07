7d8b73e16182e17bbc7482159f4bfb6e1e54213b
Richard Dennis <mad_raptor@hotmail.com>
2014-07-07 23:13:46 +0100

    Update rendFuncs.py

--- rendFuncs.py
+++ rendFuncs.py
@@ -16,7 +16,9 @@ import consensus
 import base64
 import re
 from urlparse import urlparse,urljoin,urlunsplit
+import urllib
 import urllib2
+from urllib2 import Request, urlopen, URLError, HTTPError
 import csv
 import pprint
 import sha
@@ -27,81 +29,37 @@ from base64 import b32encode, b32decode
 from random import randint
 from bisect import bisect_left
 
-def get_descriptor_Id(service_id_base32, replica, descriptor_cookie = ""):
-  service_id = b32decode(service_id_base32, 1)
-  epoch_time = int(time.time())   #epoch time  seconds since 1970
-  time_period = get_time_period(epoch_time, 0, service_id)
-  si = sha1()
-  si.update(struct.pack('>I', time_period)[:4]);
-  if descriptor_cookie:
-    si.update(descriptor_cookie)
-  si.update('{0:02X}'.format(replica).decode('hex'))
-  si = si.digest()
-  di = sha1()
-  di.update(service_id)
-  di.update(si)
-  di = di.digest()
-  return b32encode(di).lower()
-
-
-# Calculates time period - time-period = (current-time + permanent-id-byte * 86400 / 256) / 86400
-def get_time_period(time, deviation, service_id):
-  REND_TIME_PERIOD_V2_DESC_VALIDITY = 24 * 60 * 60
-  return int(((time + ((struct.unpack('B', service_id[0])[0] * REND_TIME_PERIOD_V2_DESC_VALIDITY) ) / 256) ) / REND_TIME_PERIOD_V2_DESC_VALIDITY + deviation)
+def get_descriptor_Id(onion_Add, replica):
+  service_id = b32decode(onion_Add, 1)
+  time_period = int((((time.time()) + ((struct.unpack('B', service_id[0])[0] * 86400) ) / 256) ) / 86400 + 0)
+  s = sha1()
+  s.update(struct.pack('>I', time_period)[:4]);
+  s.update('{0:02X}'.format(replica).decode('hex'))
+  s = s.digest()
+  d = sha1()
+  d.update(service_id)
+  d.update(s)
+  d = d.digest()
+  return b32encode(d).lower()
 
 def find_responsible_HSDir(descriptor_id):
    responsible_HSDirs = []
-   identity_list = []
-
    HSDir_List = consensus.get_HSDir_Flag()  # Allows us to only get the data containing the HSDir flags
-
    orHashList = sorted(map(lambda x: x['identity'], HSDir_List))
-
-   print orHashList
-
-   descriptor_position = bisect_left(orHashList, b32decode(descriptor_id,1)) #should be identiy list not HSDir_List
-   print descriptor_id
+   descriptor_position = bisect_left(orHashList, b32decode(descriptor_id,1)) #should be identiy list not HSDir_List #TODO - Add the other part of the list to it so it makes a circle
    for i in range(0,3):
       responsible_HSDirs.append(orHashList[descriptor_position+i])  
-
-      # IndexError: list index out of range  
-
    return (map(lambda x: consensus.get_router_by_hash(x) ,responsible_HSDirs))
-
- 
-
-
 
 def create_rendezvous_cookie():
    rendezvous_cookie = os.urandom(20) #Random 20 byte value
    return rendezvous_cookie
 
-
-
-
-
-
-
-
-#    #descriptor-id = H(permanent-id | H(time-period | descriptor-cookie | replica))
-#    #permanent-id = H(public-key)[:10]
-
-# #  onion = base64.b32decode(onionb64.upper())
-# #   curtime = int(parse(timestr).strftime("%s"))
-# #   replica = 0
-#    epoch_time = int(time.time())   #epoch time  seconds since 1970
-#    timePeriod = struct.pack(">IB", int((epoch_time + ord(onion[0]) * 86400 // 256) // 86400),replica)
-#    ht = sha.new()
-#    ht.update(timePeriod)
-#    hashT = ht.digest()
-#    h = sha.new()
-#    h.update(onion + hashT)
-#    return base64.b32encode(h.digest()).lower()
-
-
-# #f = urllib.urlopen('http://'+router['address']+':'+str(router['dir_port'])+'/tor/rendezvous2/'+router['descriptor_id'])
-
-
-# def rendezvous2d_compute_v2_desc_id(service_id_base32, replica, time = int(time()), descriptor_cookie = ""):
-#   service_id = b32decode(service_id_base32, 1)
-#   return service_id
+def connect_to_web_lookup(ip_addresses, dirport, descriptor_id_list):
+  web_addresses =[]
+# creates a list of all possible web addresses to connect to using the data retrieved from above
+  for i in range(0,len(descriptor_id_list)):
+    for j in range(0,3):
+      a_elem = i*3 + j
+      web_addresses.append('http://'+ip_addresses[a_elem]+':'+str(dirport[a_elem])+'/tor/rendezvous2/'+descriptor_id_list[i])
+  return web_addresses
